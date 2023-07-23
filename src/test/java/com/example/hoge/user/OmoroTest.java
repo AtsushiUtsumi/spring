eb651be190d01ae3f7f5974106e410b4df1edcd2
AtsushiUtsumi <debtgx103@gmail.com>
2023-07-23 17:54:06 +0900

    テストにも引数をとれる

---
+++ src/test/java/com/example/hoge/user/OmoroTest.java
@@ -0,0 +1,25 @@
+package com.example.hoge.user;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.boot.test.context.SpringBootTest;
+
+// クラスを右クリックで新規テストでなぜかうまく出来てしまったjpiterだからか?
+
+@SpringBootTest
+class OmoroTest {
+
+	// こういう風にテストにも引数をとれる
+	@ParameterizedTest
+	@CsvSource({
+		"1, 1, 2",
+		"1, 5, 6",
+		"1, 2, 3"
+	})
+	void テストクラスの中でAutowiredしたい(int a, int b, int expected) {
+		int x = a + b;
+		assertEquals(expected, x);
+	}
+}
